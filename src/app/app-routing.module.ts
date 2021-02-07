b071a12d716047397d78530c1f874c7383dd8de1
ashiffm21 <ashiffm21@gmail.com>
2021-02-07 14:24:29 +0530

    Authentication page and input handling added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthComponent } from './auth/auth.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -17,6 +18,8 @@ const appRoutes: Routes = [
     { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]},
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component:AuthComponent },
+
 ];
 
 @NgModule({
